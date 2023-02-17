109d5de18e102776569dc13d5b4afaa0aa0e3d57
Brian Cavalier <brian@briancavalier.com>
2023-02-17 09:44:41 -0500

    Add some tests

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * Immutable subset of builtin Set.
+ * Immutable view of builtin Set.
  * Note that this allows Set to be passed directly to functions accepting
  * ImmutableSet, but *not* vice versa.
  */
@@ -25,15 +25,16 @@ export const empty: ImmutableSet<never> = new Set()
 export const isEmpty = <A>(s: ImmutableSet<A>): s is ImmutableSet<never> => s.size === 0
 
 /**
- * Return true if and only if `a` is member of `s`
+ * Return true if and only if `a` is member of `s`. Membership is determined
+ * using the same semantics as {@link Set}.
  */
 export const has = <A>(a: A, s: ImmutableSet<A>): boolean => s.has(a)
 
 /**
  * Create an {@link ImmutableSet} containing the provided elements
  */
-export const of = <Elements extends readonly unknown[]>(...[e0, ...rest]: Elements): ImmutableSet<Elements[number]> =>
-  new Set(rest).add(e0)
+export const of = <Elements extends readonly unknown[]>(...elements: Elements): ImmutableSet<Elements[number]> =>
+  new Set(elements)
 
 export type Union<Sets extends readonly ImmutableSet<unknown>[]> = ElementType<Sets[number]>
 
@@ -72,10 +73,10 @@ export const symmetric = <A, B>(s0: ImmutableSet<A>, s1: ImmutableSet<B>): Immut
 
 /**
  * True if and only if S0 and S1 represent the same set of elements. Element
- * equality is determined using `===`.
+ * equality is determined using the same semantics as {@link Set}.
  */
 export const equal = <S0 extends ImmutableSet<unknown>, S1 extends S0>(s0: S0, s1: S1): s0 is S1 =>
-  s0.size === s1.size && [...s0].every(x => s1.has(x))
+  s0 === s1 || (s0.size === s1.size && [...s0].every(x => s1.has(x)))
 
 /**
  * Apply f to all the elements of s
@@ -88,7 +89,3 @@ export const map = <A, B>(f: (a: A) => B, s: ImmutableSet<A>): ImmutableSet<B> =
  */
 export const filter = <A, B extends A>(p: (a: A) => a is B, s: ImmutableSet<A>): ImmutableSet<B> =>
   new Set([...s].filter(p))
-
-export type SetsOf<A extends readonly unknown[]> = {
-  [K in keyof A]: ImmutableSet<A[K]>
-}
